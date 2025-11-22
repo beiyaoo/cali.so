2913eb23469729119e47ba061e8df7a7783fca78
beiyaoo <2383615282@qq.com>
2025-11-22 18:46:39 +0800

    修改图片

--- app/(main)/confirm/[token]/page.tsx
+++ app/(main)/confirm/[token]/page.tsx
@@ -37,7 +37,7 @@ export default async function ConfirmPage({
           className="w-full text-center text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
           id="subbed-celebration"
         >
-          🥳 感谢你的订阅 🎉
+          🥳🥳 感谢你的订阅🎉 🎉
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400"></p>
       </header>
